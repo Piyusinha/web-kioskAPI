eb494304db04ff2c6db1cda621f381c1e20f61c7
piyush <piyush.sinha12345@gmail.com>
2019-11-09 12:59:29 +0530

    marks-added

--- app.py
+++ app.py
@@ -6,33 +6,34 @@ import sys
 from flask import jsonify
 
 app =Flask(__name__)
-resp = urllib.request.urlopen("https://webkiosk.jiit.ac.in/index.jsp")
-soup = BeautifulSoup(resp,'html.parser')
-captcha=soup.find_all('i')[0].text
-header = resp.info()
-header = header.items()[2][1]
-header, sep, tail = header.partition(';')
-head, sep, jSesId = header.partition('=')
 
-cookies='JSESSIONID='+jSesId+';switchmenu='
-timeout=10
-headers = {
-'Host': 'webkiosk.jiit.ac.in',
-'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0',
-'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,/;q=0.8',
-'Accept-Language': 'en-US,en;q=0.5',
-'Accept-Encoding': 'gzip, deflate, br',
-'Content-Type': 'application/x-www-form-urlencoded',
-'Cookie':cookies,
-'DNT': '1',
-'Connection': 'keep-alive',
-'Upgrade-Insecure-Requests': '1',
-'Content-length':'248',
-'Referer': 'https://webkiosk.jiit.ac.in/'
-
-}
 @app.route('/attendance/<string:enrollment>/<string:dob>/<string:pa>')
 def fetchattendance(enrollment,dob,pa):
+    resp = urllib.request.urlopen("https://webkiosk.jiit.ac.in/index.jsp")
+    soup = BeautifulSoup(resp,'html.parser')
+    captcha=soup.find_all('i')[0].text
+    header = resp.info()
+    header = header.items()[2][1]
+    header, sep, tail = header.partition(';')
+    head, sep, jSesId = header.partition('=')
+
+    cookies='JSESSIONID='+jSesId+';switchmenu='
+    timeout=10
+    headers = {
+    'Host': 'webkiosk.jiit.ac.in',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0',
+    'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,/;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Cookie':cookies,
+    'DNT': '1',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+    'Content-length':'248',
+    'Referer': 'https://webkiosk.jiit.ac.in/'
+
+    }
     payload = {
     "x":"",
     "txtInst":"Institute",
@@ -64,6 +65,31 @@ def fetchattendance(enrollment,dob,pa):
     return jsonify(a)
 @app.route('/marks/<string:enrollment>/<string:dob>/<string:pa>')
 def marks(enrollment,dob,pa):
+    resp = urllib.request.urlopen("https://webkiosk.jiit.ac.in/index.jsp")
+    soup = BeautifulSoup(resp,'html.parser')
+    captcha=soup.find_all('i')[0].text
+    header = resp.info()
+    header = header.items()[2][1]
+    header, sep, tail = header.partition(';')
+    head, sep, jSesId = header.partition('=')
+
+    cookies='JSESSIONID='+jSesId+';switchmenu='
+    timeout=10
+    headers = {
+    'Host': 'webkiosk.jiit.ac.in',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0',
+    'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,/;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Cookie':cookies,
+    'DNT': '1',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+    'Content-length':'248',
+    'Referer': 'https://webkiosk.jiit.ac.in/'
+
+    }
     payload = {
     "x":"",
     "txtInst":"Institute",
